b7717cce10e5805cf97743475f38360cd987fc9f
Your Name <you@example.com>
2024-01-25 13:09:17 +0530

    identifier

---
+++ identifiers.js
@@ -0,0 +1,21 @@
+// creat
+
+var brand='p'
+var pname="Mens check shirt"
+var price=499
+var rateig = 3.7
+var avaliblty= true
+var discount ;
+var loc=null
+
+
+// print data types
+console.log(typeof brand)
+console.log(typeof p_name)
+console.log(typeof price)
+console.log(typeof rateing)
+console.log(typeof avaliblty)
+console.log(typeof discount)
+console.log(typeof  loc)
+
+console.log (pname)
--- rajesh.js
+++ rajesh.js
@@ -1,3 +1,3 @@
 var a = 10
 var b = 20
-console.log (a+b)
+console.log (a+b) 
