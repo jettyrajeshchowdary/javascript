caf1745e76e62cf314442408b2bc273d4aa358df
Your Name <you@example.com>
2024-01-26 12:19:04 +0530

    .

--- js/non-primitive.js
+++ js/non-primitive.js
@@ -7,7 +7,7 @@ var specification = {
 
 let size = [38,40,42,44,46,48,50]
 let emp = {"id":104,
-                    "name" :"Sunil",
+                    "name" : "Sunil",
                     "sal": 44000
           }  
 
