c4dcf0dd48828d20263411faa2c0d28e93e25d3b
Your Name <you@example.com>
2024-02-12 19:20:19 +0530

    test complet

---
+++ JS3/OOPS/three.js
@@ -0,0 +1,16 @@
+class Employee{
+    eid=101;
+    ename="sunil rajesh shanbor"
+  
+    get_Details(){
+
+    }
+}
+let e1=new Employee()
+let e2=new Employee()
+let e3=new Employee()
+
+
+console.log(e1)
+console.log(e2)
+console.log(e3)
