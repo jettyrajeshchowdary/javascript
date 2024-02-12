9109ca5871eb8d50b08fe7923d35820b7d44bf58
Your Name <you@example.com>
2024-02-12 19:12:29 +0530

    test complet

---
+++ JS3/OOPS/four.js
@@ -0,0 +1,23 @@
+class Account{
+    acc_id=101;
+    acc_name='shanbor'
+    acc_bal=500
+
+    open_Account(){
+
+    }
+    deposit_Amount(){
+
+    }
+    withdrawl(){
+
+    }
+    get_Bal(){
+
+    }
+    close_Account(){
+
+    }
+}
+let a1=new Account()
+console.log(a1)
