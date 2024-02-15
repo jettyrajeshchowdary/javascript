4fe79345be6a8771f9f35bafc50bded3f617c27f
Your Name <you@example.com>
2024-02-15 11:54:37 +0530

    test complete

--- JS3/OOPS/CLASS/employee.js
+++ JS3/OOPS/CLASS/employee.js
@@ -1,12 +1,12 @@
 class Employee{
     eid =101;
-    ename;
+    ename="rajesh sunil shanbor";
     get_Details(){}
 } 
 
-let el = new Employee;
-let e2= new Employee;
-let e3 = new Employee;
+let e1 = new Employee();
+let e2= new Employee();
+let e3 = new Employee();
 
 console.log(e1);
 console.log(e2);
