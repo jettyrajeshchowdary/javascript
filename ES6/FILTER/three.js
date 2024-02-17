0787dce127871b006be7229832fb805d05a40cc5
Your Name <you@example.com>
2024-02-17 13:36:51 +0530

    test complet

---
+++ ES6/FILTER/three.js
@@ -0,0 +1,6 @@
+let employees=["sunil","sonia","priyanka"]
+
+let new_employees=employees.filter(function(emp_name){
+      return emp_name.length>6
+})
+console.log(new_employees)
---
+++ ES6/FILTER/two.js
@@ -0,0 +1,10 @@
+/* let numbers=[1,2,3,4,5,6,7,8,9,10]
+
+let evens=numbers.filter(nummber=>number%2==0)
+console.log(evens)
+ */
+
+let numbers=[1,2,3,4,5,6,7,8,9,10]
+
+let odds=numbers.filter(number=>number%2==1)
+console.log(odds)
